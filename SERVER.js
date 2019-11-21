06dc7f73e1d2d2fc26ddbe3f7e9fe4b245e394be
= <=>
2019-11-21 13:29:32 +0700

    21 Nov

--- SERVER.js
+++ SERVER.js
@@ -6,8 +6,31 @@ app.set("views", "./views");
 var server = require("http").Server(app);
 var io = require("socket.io")(server);
 server.listen(3000);
+
+var arrUser = [];
+
 io.on("connection", function(socket){
 console.log("connected "+socket.id);
+socket.on("client-send-Username", function(data){
+  if(arrUser.indexOf(data)>=0){
+    //emit register failure
+    socket.emit("server-send-fail");
+  }
+  else{
+    arrUser.push(data);
+    socket.username = data;
+    // emit register succesful
+    socket.emit("server-send-succesful", data);
+    io.sockets.emit("server-send-list-user",arrUser);
+  }
+});
+socket.on("client-send-logOut", function(){
+  arrUser.splice(arrUser.indexOf(socket.username),1);
+  socket.broadcast.emit("server-send-list-user",arrUser);
+});
+socket.on("user-send-message", function(data){
+  io.sockets.emit("server-send-message", {user:socket.username, content:data});
+});
 });
 app.get("/", function(req, res){
   res.render("home")
--- public/script.js
+++ public/script.js
@@ -1,5 +1,40 @@
 var socket = io("http://localhost:3000");
+socket.on("server-send-fail", function(){
+  alert("Register fail, username has been exist");
+});
+
+socket.on("server-send-list-user", function(data){
+  $("#contentBox").html("");
+  data.forEach(function(i){
+    $("#contentBox").append("<div class='user'>"+i+"</div");
+  });
+});
+
+socket.on("server-send-succesful", function(data){
+  $("#loginForm").hide(2000);
+  $("#chatForm").show(1000);
+  $("#currentUser").html(data);
+});
+socket.on("server-send-message", function(data){
+  $("#listMessage").append("<div class='message'>"+ data.user+": "+data.content+"</div>");
+});
 $(document).ready(function(){
   $("#loginForm").show();
   $("#chatForm").hide();
-})
+
+  //click register
+  $("#btnRegister").click(function(){
+    socket.emit("client-send-Username", $("#txtUsername").val());
+
+  });
+
+$("#logOut").click(function(){
+  socket.emit("client-send-logOut");
+  $("#loginForm").show(2000);
+  $("#chatForm").hide(1000);
+});
+$("#btnSendMessage").click(function(){
+  socket.emit("user-send-message", $("#txtMessage").val());
+});
+
+});
